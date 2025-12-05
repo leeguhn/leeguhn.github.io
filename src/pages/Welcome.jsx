78b643812bcbd8fdb33dfee5baea3fa8c6eac778
tommyglee <50029053+leeguhn@users.noreply.github.com>
2025-12-05 19:18:09 +0900

    5 minutes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -30,7 +30,7 @@ const Welcome = () => {
           You sent $50 to 'Mom' yesterday, but she hasn't received it. You will need to verify your identity and provide details. Please type your answers naturally.
         </p>
         <p className="welcome-description" style={{fontWeight: '500', color: 'red'}}>
-          This simulation is not intended for mobile. Please use a desktop or laptop for the best experience. The entire study takes approximately 5-10 minutes to complete. 
+          This simulation is not intended for mobile. Please use a desktop or laptop for the best experience. The entire study takes approximately 5 minutes to complete. 
         </p>
         
         <form onSubmit={handleSubmit} className="welcome-form">
